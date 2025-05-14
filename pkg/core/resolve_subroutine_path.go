3426ffc85e952c8d0f0f201f48b7810f679a4495
Jaakko Heusala <jhh@hg.fi>
2025-05-14 20:37:06 +0300

    Added documentation comments

--- pkg/core/resolve_subroutine_path.go
+++ pkg/core/resolve_subroutine_path.go
@@ -1,17 +1,20 @@
 package core
 
 import (
-	"github.com/hyperifyio/gnd/pkg/units"
 	"io/fs"
 	"os"
 	"path/filepath"
+
+	"github.com/hyperifyio/gnd/pkg/units"
 )
 
 // ResolveSubroutinePath checks if the opcode is a subroutine and returns its path
 func ResolveSubroutinePath(opcode string, scriptDir string) (bool, string) {
+	// First check in the script directory
 	if _, err := os.Stat(filepath.Join(scriptDir, opcode+".gnd")); err == nil {
 		return true, opcode + ".gnd"
 	}
+	// Then check in the embedded filesystem (which is already rooted at gnd/)
 	if _, err := fs.Stat(units.GetUnitsFS(), opcode+".gnd"); err == nil {
 		return true, opcode + ".gnd"
 	}
